3a5020752f920a4118d49c9578113814434c3729
Amit Joseph <amit.joseph1@mail.huji.ac.il>
2023-09-13 14:51:01 +0300

    Cannot send gmail using python

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -1,7 +1,10 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt 
 from flask_login import LoginManager
+from flask_mail import Mail
 
 
 
@@ -12,6 +15,16 @@ db = SQLAlchemy(app)
 bycrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
+
+##########################################################
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT']=587
+app.config['MAIL_USE_TLS']=True
+app.config['MAIL_USERNAME']=os.getenv('EMAIL_USER')
+app.config['MAIL_PASSWORD']=os.getenv('EMAIL_PASS')
+mail = Mail(app)
+###########################################################
+
 
 from flask_blog import routes
--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -2,10 +2,11 @@ import secrets
 import os
 from PIL import Image
 from flask import render_template, url_for, flash, redirect,request,abort
-from flask_blog import app, db, bycrypt
-from flask_blog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
+from flask_blog import app, db, bycrypt, mail
+from flask_blog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm ,RequestResetForm , ResetPasswordForm
 from flask_blog.models import User, Post
 from flask_login import login_user, logout_user, current_user, login_required
+from flask_mail import Message
 
 
 
@@ -169,6 +170,55 @@ def user_posts(username):
     user = User.query.filter_by(username = username).first_or_404()
     posts = Post.query.filter_by(author=user)\
         .order_by(Post.date_posted.desc())\
-        .paginate(page = page ,per_page=5) # '\' able us to break line
+        .paginate(page = page ,per_page=5) # paginate able to sparate for pages
     return render_template('user_posts.html',posts = posts, user = user)
 
+
+
+
+
+################################################################################
+def send_reset_email(user):
+    token = user.get_reset_token()
+    msg = Message('Password Reset Requset', sender='amitj.you98@gmail.com', recipients=[user.email])
+    msg.body = f''' To reset your password, visit the following link: 
+{url_for('reset_token', token = token, external = True)}
+'''
+    mail.send(msg)
+
+
+
+@app.route("/reset_password", methods=['GET', 'POST'])
+def reset_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
+    form = RequestResetForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email = form.email.data).first()
+        send_reset_email(user)
+        flash('An email has been sent with instructions to reset your password','info')
+        return redirect(url_for('login'))
+    return render_template('reset_request.html', title = 'Reset Password', form = form)
+
+
+
+@app.route("/reset_password/<token>", methods=['GET', 'POST'])
+def reset_token(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
+    user = User.verify_reset_token(token)
+    if user == None:
+        flash('That is an invalid/expired token','warning')
+        return redirect(url_for('reset_request'))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        hashed_password = bycrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user.password = hashed_password
+        db.session.commit()
+        flash(f'Your Password had been updated!!','success')
+        return redirect(url_for('login'))
+    return render_template('reset_token.html', title = 'Reset Password', form = form)
+
+#######################################################################################3
